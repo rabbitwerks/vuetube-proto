ce7ee5737c302e2f77d57b684cd243a289400b61
SeanBecker <techtronixwebdesign@gmail.com>
2019-04-20 22:45:17 -0500

    update: app.vue, main.css, google-font, packages installed, basic layout started

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Vuetube</h1>
+  <div class="app--container--shell">
+    <main--container />
   </div>
 </template>
 
 <script>
+import './assets/main.css';
+
+import Main__Container from './views/Main'
+
+import API_KEY from '../env.js';
+
 
 export default {
   name: 'app',
+  components: {
+    'main--container': Main__Container
+  },
+  data() {
+    return {
+      data: {
+        channel_id: 'UCyH4i2twA5tdJhZVcSoNZRQ',
+        channelDataRaw: [],
+        channelData: {
+          snippet: {},
+          contentDetails: {},
+          statistics: {},
+        }
+      }
+    }
+  },
+  // created() {
+  //   const { channel_id, channelData } = this.data;
+  //   // console.log('Main API', api);
+  //   console.log('Channel', api.searchChannel(channel_id));
+  //   // console.log('Channel Sections', api.searchChannelSections(channel_id));
+  //   api.searchChannel(channel_id)
+  //     .then(data => {
+  //       this.data.channelDataRaw.push(data.items[0])
+  //       const { snippet, contentDetails, statistics } = data.items[0];
+  //       console.log(snippet)
+  //       this.data.channelData.snippet = snippet;
+  //       this.data.channelData.contentDetails = contentDetails;
+  //       this.data.channelData.statistics = statistics;
+
+  //     }).catch(error => console.log(error))
+  // },
+  
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
