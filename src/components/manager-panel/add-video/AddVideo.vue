f4bbac2d08a4b5fcd19a4cada5353ecd7e640dd5
SeanBecker <techtronixwebdesign@gmail.com>
2019-04-27 21:51:23 -0500

    add: video-preview markup to-be componentized

--- src/components/manager-panel/add-video/AddVideo.vue
+++ src/components/manager-panel/add-video/AddVideo.vue
@@ -11,29 +11,60 @@
         </div>
       </div>
 
-      <div class="add-video--inner">
+      <div class="add-video--inner flexbox flexdir-col">
 
         <div class="input-group flexbox flexdir-col">
-          <label class="input-label" for="url-input">Video URL</label>
+          <label class="input-label" for="url-input"
+            >Video URL
+          </label>
           <input type="text" class="input-text" id="url-input"/>
         </div>
 
         <div class="input-group flexbox flexdir-col">
-
-          <label class="input-label" for="playlist-dropdown">Select Playlist</label>
+          <label class="input-label" for="playlist-dropdown"
+            >Select Playlist
+          </label>
           <div class="add-to-playlist--group flexbox flexgap-2">
 
             <select id="playlist-dropdown" class="input-text flex-5">
               <option value="playlist00">Vue Basics</option>
               <option value="playlist01">Modern Javascript</option>
               <option value="playlist02">My First Playlist</option> 
-              <option value="new-playlist">My First Playlist</option> 
+              <option class="dd-new-playlist" value="new-playlist">- - Create New Playlist</option> 
             </select>
 
             <button class="button action add-video--button flex-1">Add</button>
 
           </div>
         </div>
+
+        <div class="preview--outer" v-if="0">
+          <label class="preview--label"
+            >Video Preview
+          </label>
+          <div class="preview--inner secondary flexbox flexgap-1">
+            
+            <div class="thumbnail--outer flexbox align-end">
+              <img src="https://i.ytimg.com/vi/pmOvwfRMXy0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAEUX-bHrWzZaYpQOwpwWVGEsTZww" alt="" class="thumbnail">
+            </div>
+
+            <div class="preview--details">
+              <div class="preview--video-title">
+                Vue.js Basics - Episode 02
+              </div>
+              <div class="preview--channel-title">
+                RabbitWerks Javascript
+              </div>
+              <div class="preview--description--outer">
+                <span class="preview--description--inner">
+                  This video is going to talk about all the cool stuff we can do with Vue. We will only be displaying 2 lines of the description here.
+                </span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   </div>
@@ -46,6 +77,11 @@ export default {
       panelIsOpen: true,
       videoURL: '',
       playlist: '',
+      videoPreview: {
+        isShowingPreview: true,
+        title: '',
+
+      }
     }
   }
 }
@@ -61,6 +97,7 @@ export default {
   height: 100%;
   background-color: var(--dark);
 }
+
 .panel-header--outer {
   height: 3rem;
   padding: 0 1rem;
@@ -70,9 +107,11 @@ export default {
   font-size: 1.75rem;
   font-weight: 700;
 }
+
 .add-video--inner {
   padding: 1rem;
 }
+
 .input-group {
   width: 100%;
   margin-bottom: 1rem;
@@ -81,16 +120,44 @@ export default {
   width: 100%;
   margin-bottom: .25rem;
 }
-.input-label {
+.input-label, .preview--label {
   margin-bottom: .25rem;
   font-size: 1.25rem;
-  color: #f7f7f7;
+  color: var(--text-light);
 }
 .input-text {
   height: 2rem;
   padding: .25rem;
   font-size: 1.25rem;
   font-family: 'Cuprum';
+}
+
+
+.preview--inner {
+  padding: .5rem;
+}
+.thumbnail {
+  width: 8rem;
+  background: #fff;
+}
+.preview--details {
+  width: 100%;
+}
+.preview--video-title {
+  font-size: 1.3rem;
+  border-bottom: 2px solid black;
+}
+.preview--channel-title {
+  margin-top: .15rem;
+  margin-bottom: -.25rem;
+  font-size: .8rem;
+}
+.preview--description--outer {
+  margin-top: .25rem;
+  font-size: .65rem;
+  background-color: #fff;
+  padding: .15rem;
+  height: 2rem;
 }
 </style>
 
