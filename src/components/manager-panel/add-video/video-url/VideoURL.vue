99d4c148ce938508145e0a994700d8a81486a794
SeanBecker <techtronixwebdesign@gmail.com>
2019-04-28 04:24:51 -0500

    add: video url component

---
+++ src/components/manager-panel/add-video/video-url/VideoURL.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-group flexbox flexdir-col">
+    <label class="input--label" for="url-input"
+      >Video URL
+    </label>
+    <input 
+      type="text" 
+      id="url-input"
+      class="input--text"
+    /> 
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input-group {
+  width: 100%;
+  margin-bottom: .75rem;
+}
+.input-group:last-of-type {
+  width: 100%;
+  margin-bottom: .25rem;
+}
+.input--label {
+  margin-bottom: .25rem;
+  font-size: 1.25rem;
+  color: var(--text-light);
+}
+</style>
