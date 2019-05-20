abb809cd205fa7a33ee79f64a0959157429ac977
SeanBecker <techtronixwebdesign@gmail.com>
2019-05-19 19:10:47 -0500

    add: Vuex Store start; video fetch handled in vuex action; setting response in state; getter for sending fetched video data to Vue

---
+++ src/store.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import Vue from 'vue';
+import Vuex from 'vuex';
+import API_KEY from '../env';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    addVideo: {
+      fetchedVideoData: {},
+      dataLoaded: false,
+    },
+    playlists: [],
+
+  },
+  getters: {
+    previewData_GET: state => {
+      return state.addVideo.fetchedVideoData;
+    }
+  },
+  mutations: {
+    fetchVideoData_MUTA(state, payload) {
+      state.addVideo.fetchedVideoData = payload;
+      state.addVideo.dataLoaded = true;
+    }
+  },
+  actions: {
+    fetchVideoData_ACTION({ commit }, payload) {
+      // fetch data - first param, search url from endpoints.js
+      // we will want to extract this out to a backend endpoint
+      // the backend would hold our sensitive credentials
+      const extractedVideoID = payload;
+      fetch(`https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&id=${extractedVideoID}&part=snippet,contentDetails,status,statistics,player`)
+      .then(res => res.json())
+      .then(videoData => {
+        commit('fetchVideoData_MUTA', videoData.items[0])
+      })
+      .catch(error => console.error(error));
+    }
+  },
+});
+
+export default store;
